bdc88e7857c2f4edc7e292b833535505713fc482
向军 <2300071698@qq.com>
2016-10-23 11:14:19 +0800

    更新框架

--- vendor/houdunwang/framework/src/model/Model.php
+++ vendor/houdunwang/framework/src/model/Model.php
@@ -342,8 +342,9 @@ class Model implements ArrayAccess, Iterator {
 
 	/**
 	 * 批量设置做准备数据
-	 *
 	 * @param array $data
+	 *
+	 * @return $this
 	 */
 	final public function create( array $data = [ ] ) {
 		if ( ! empty( $data ) ) {
@@ -373,13 +374,15 @@ class Model implements ArrayAccess, Iterator {
 			$this->original[ $this->pk ] = $this->data[ $this->pk ];
 		}
 		//修改时间
-		if ( $this->timestamps === true ) {
+		if ( $this->timestamps === true && ! empty( $this->original ) ) {
 			$this->original['updated_at'] = NOW;
 			if ( $this->actionType() == self::MODEL_INSERT ) {
 				//更新时间
 				$this->original['created_at'] = NOW;
 			}
 		}
+
+		return $this;
 	}
 
 	/**
@@ -395,8 +398,8 @@ class Model implements ArrayAccess, Iterator {
 	 * @return bool
 	 */
 	final public function touch() {
-		if ( $this->actionType() == self::MODEL_UPDATE ) {
-			return Db::table( $this->table )->where( $this->pk, $this->data[ $this->pk ] )->update( [ 'update_at' => NOW ] );
+		if ( $this->actionType() == self::MODEL_UPDATE && $this->timestamps ) {
+			return Db::table( $this->table )->where( $this->pk, $this->data[ $this->pk ] )->update( [ 'updated_at' => NOW ] );
 		}
 	}
 
