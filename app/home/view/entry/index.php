be1cd892d8717b7b69812d3e909dafd4576530a1
向军 <2300071698@qq.com>
2016-10-24 21:57:00 +0800

    修改默认模板

--- app/home/view/entry/index.php
+++ app/home/view/entry/index.php
@@ -8,5 +8,6 @@
 <body style="background: #f3f3f3;">
 <h1 class="text-muted text-center" style="margin-top:200px;font-size:80px;">HDPHP {{HDPHP_VERSION}}</h1>
 <h4 class="text-muted text-center">服务化/组件化/模块化的未来框架产品</h4>
+<h6 class="text-muted text-center">framework: {{FRAMEWORK_VERSION}}</h6>
 </body>
 </html>
