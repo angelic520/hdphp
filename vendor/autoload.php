d6ef8c7c06ca7519f4f93ddba3a65700f9ca6b36
向军 <2300071698@qq.com>
2016-10-27 21:34:31 +0800

    更新核心包

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInit25b1f7e4e5278cea8f4516ab561dcc72::getLoader();
+return ComposerAutoloaderInitf584305b151cc9471ac609deec7e3564::getLoader();
