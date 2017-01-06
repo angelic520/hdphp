4b3a42b3c51ce1ee459d9c4153d70392c806fd1d
houdunwang <2300071698@qq.com>
2017-01-07 03:48:20 +0800

    no message

--- system/hdphp.php
+++ system/hdphp.php
@@ -1,15 +1,15 @@
 <?php
 /*--------------------------------------------------------------------------
-| Software: [HDPHP framework]
-| Site: www.hdphp.com
-|--------------------------------------------------------------------------
-| Author: 向军 <2300071698@qq.com>
-| WeChat: aihoudun
-| Copyright (c) 2012-2019, www.houdunwang.com. All Rights Reserved.
-|-------------------------------------------------------------------------*/
-
+| PHP版本检测
+|--------------------------------------------------------------------------*/
 if ( version_compare( phpversion(), '5.4.0', '<' ) ) {
 	die( '<h1 style=\'margin:20px;color:#535353;font:36px/1.5 Helvetica, Arial\'><span style="font-size:150px;">:( </span><br/>Please upgrade to PHP5.4 above</h1>' );
 }
+
+/*--------------------------------------------------------------------------
+| 框架版本号
+|--------------------------------------------------------------------------*/
+define( 'HDPHP_VERSION', '3.0.20' );
+
 require __DIR__ . '/../vendor/autoload.php';
 \houdunwang\framework\App::bootstrap();
