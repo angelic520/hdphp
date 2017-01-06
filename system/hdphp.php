584ac9a78e129300629927e65df836d9245337bb
houdunwang <2300071698@qq.com>
2017-01-07 03:42:00 +0800

    no message

--- system/hdphp.php
+++ system/hdphp.php
@@ -1,19 +1,15 @@
 <?php
-// .-------------------------------------------------------------------
-// |  Software: [HDPHP framework]
-// |      Site: www.hdphp.com
-// |-------------------------------------------------------------------
-// |    Author: 向军 <2300071698@qq.com>
-// |    WeChat: houdunwangxj
-// | Copyright (c) 2012-2019, www.houdunwang.com. All Rights Reserved.
-// '-------------------------------------------------------------------
-header( "Content-type:text/html;charset=utf-8" );
+/*--------------------------------------------------------------------------
+| Software: [HDPHP framework]
+| Site: www.hdphp.com
+|--------------------------------------------------------------------------
+| Author: 向军 <2300071698@qq.com>
+| WeChat: aihoudun
+| Copyright (c) 2012-2019, www.houdunwang.com. All Rights Reserved.
+|-------------------------------------------------------------------------*/
+
 if ( version_compare( phpversion(), '5.4.0', '<' ) ) {
-	die( '<h1 style=\'margin:20px;color:#535353;font:36px/1.5 "PingFang SC", Helvetica, "Helvetica Neue", "微软雅黑", Tahoma, Arial, sans-serif\'><span style="font-size:150px;">:( </span><br/>PHP要求版本>=5.4</h1>' );
+	die( '<h1 style=\'margin:20px;color:#535353;font:36px/1.5 Helvetica, Arial\'><span style="font-size:150px;">:( </span><br/>Please upgrade to PHP5.4 above</h1>' );
 }
-//composer自动加载
 require __DIR__ . '/../vendor/autoload.php';
-define( 'ROOT_PATH', dirname( __DIR__ ) );
-define( 'HDPHP_VERSION', '3.0.20' );
-//启动应用
 \houdunwang\framework\App::bootstrap();
